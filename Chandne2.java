c0b6c1001e798d6f6693578d980a93fa0dabd0f8
Shreya Rajesh Chandne <123990729+shreya0077@users.noreply.github.com>
2023-02-09 14:31:33 +0530

    practice on 05-02

---
+++ Chandne2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Chandne2
+{
+      public static void main(String[] args) {
+
+
+         int a,b;
+         System.out.println("Enter two Numbers");
+         Scanner r=new Scanner(System.in);
+         a=r.nextInt();
+         b=r.nextInt();
+         if(a>b)
+         {
+
+            System.out.println(a);
+         }
+         else
+         {
+      
+            System.out.println(b);
+         }
+      
+      }  
+
+}
+
